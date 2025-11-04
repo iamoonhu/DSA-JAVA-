5ca40b062ea568d1e2f1f7445b958564cfab049c
Chandramani <cjha12110@gmail.com>
2025-11-04 22:50:18 +0530

    Update Array06.java
    This is KADANE's algorithm for finding maximum sum of sub array
    , Which is an optimal solution with T/C = O(n).

--- Array06.java
+++ Array06.java
@@ -29,10 +29,23 @@ public class Array06 {
         System.out.println("Min sum of the subarray is-->"+max);
         System.out.println("total number of sub arrays are-->"+ts);
     }
+    public static  void KadansAlgorithm(int num[]){
+        int ms=Integer.MIN_VALUE;
+        int cs=0;
+        for (int i=0; i< num.length;i++){
+            cs= cs+num[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms=Math.max(cs,ms);
+        }
+        System.out.println("max sum of the subarray of the given array is-->"+ms);
+    }
 
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7};
-        Array06.subarrays(arr);
+        Array06.KadansAlgorithm(arr);
 
     }
 }
+
